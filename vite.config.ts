6e2fea40a9767a4cee7fc68b32d78775ed946c6f
jeffjr2060 <kevinchela714@gmail.com>
2023-05-30 01:29:52 +0300

    Setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/holly-react/',
+  base: '/template-react-main/',
   build: {
     outDir: '../dist',
   },
